c41496b53a880c00cb958441a0ed4aa4b3087f25
shatharababah22 <shatha.rababah22@gmail.com>
2024-09-14 14:50:49 +0300

    shatha

--- yii/common/models/UploadForm.php
+++ yii/common/models/UploadForm.php
@@ -37,11 +37,15 @@ class UploadForm extends \yii\db\ActiveRecord
     public function upload()
     {
         if ($this->validate()) {
-            $this->imageFile->saveAs( 'images/'. $this->imageFile->baseName . '.' . $this->imageFile->extension);
+ 
+            $fileName = $this->imageFile->baseName . '_' . time() . '.' . $this->imageFile->extension;
+         
+            $this->imageFile->saveAs('images/' . $fileName);
             return true;
         } else {
             return false;
         }
     }
+    
     }
 
